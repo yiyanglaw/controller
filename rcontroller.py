7b8b73b6b1ba9ac12069da6a030719d471af904a
lyxc <103878931+yiyanglaw@users.noreply.github.com>
2023-12-20 11:36:55 +0800

    Add files via upload

--- rcontroller.py
+++ rcontroller.py
@@ -10,6 +10,7 @@ import time
 
 class RobotControllerApp:
     def __init__(self, root):
+        GPIO.setmode(GPIO.BCM)
         self.root = root
         self.root.title("Robot Controller App")
         self.root.geometry("680x400")
@@ -70,6 +71,14 @@ class RobotControllerApp:
         self.IN4 = 26
         self.ENA = 16
         self.ENB = 13
+        
+        # Definition of buzzer pin
+        self.buzzer = 8
+
+        # Set up buzzer pin
+        GPIO.setup(self.buzzer, GPIO.OUT)
+        GPIO.output(self.buzzer, GPIO.HIGH)  # Initially set the buzzer to HIGH (off)
+
 
         # Motor control pins initialization
         self.motor_init()
@@ -179,8 +188,11 @@ class RobotControllerApp:
         button13 = tk.Button(self.menu_frame, text="BLUE", font=("Helvetica", 12), command=self.button13_action,width=7, height=3)
         button13.place(x=1000, y=546)
         
+        button14 = tk.Button(self.menu_frame, text="SPIN L", font=("Helvetica", 12), command=self.button14_action,width=7, height=3)
+        button14.place(x=1100, y=546)
         
-        
+        button15 = tk.Button(self.menu_frame, text="SPIN R", font=("Helvetica", 12), command=self.button15_action,width=7, height=3)
+        button15.place(x=1200, y=546)
 
         
         # Create menus
@@ -191,9 +203,9 @@ class RobotControllerApp:
         main_menu.add_cascade(label="Robot Programs", menu=robot_programs_menu)
 
         # Add options to the "Robot Programs" menu
-        robot_programs_menu.add_command(label="Option 1", command=self.robot_program_1_action)
-        robot_programs_menu.add_command(label="Option 2", command=self.robot_program_2_action)
-        robot_programs_menu.add_command(label="Option 3", command=self.robot_program_3_action)
+        robot_programs_menu.add_command(label="Black Line Tracking", command=self.robot_program_1_action)
+        robot_programs_menu.add_command(label="Light Following", command=self.robot_program_2_action)
+        robot_programs_menu.add_command(label="RGB_LED", command=self.robot_program_3_action)
         robot_programs_menu.add_command(label="Option 4", command=self.robot_program_4_action)
         robot_programs_menu.add_command(label="Option 5", command=self.robot_program_5_action)
         robot_programs_menu.add_command(label="Option 6", command=self.robot_program_6_action)
@@ -210,6 +222,8 @@ class RobotControllerApp:
 
     # Define the functions for the buttons
     def button1_action(self):
+        # Set the initial speed
+        self.speed = 50
         self.move_forward()
 
     def button2_action(self):
@@ -226,14 +240,30 @@ class RobotControllerApp:
 
         # Define the functions for the additional buttons
     def button6_action(self):
-        messagebox.showinfo("Button ACC", "Button 6 clicked")
+        # Check if the speed has been initialized by clicking the "Move Forward" button
+        if hasattr(self, 'speed'):
+            # Accelerate by increasing the speed
+            self.speed += 10
+            if self.speed > 100:
+                self.speed = 100  # Limit the speed to 100
+            self.move_forward()
+        else:
+            messagebox.showwarning("Warning", "Please click 'Move Forward' first to set the initial speed.")
 
     def button7_action(self):
-        messagebox.showinfo("Button DEC", "Button 7 clicked")
+        # Check if the speed has been initialized by clicking the "Move Forward" button
+        if hasattr(self, 'speed'):
+            # Decelerate by decreasing the speed
+            self.speed -= 10
+            if self.speed < 0:
+                self.speed = 0  # Limit the speed to 0
+            self.move_forward()
+        else:
+            messagebox.showwarning("Warning", "Please click 'Move Forward' first to set the initial speed.")
 
     def button8_action(self):
-        messagebox.showinfo("Button BEEP", "Button 8 clicked")
-
+        self.whistle()
+        
     def button9_action(self):
         messagebox.showinfo("Button MODE", "Button 9 clicked")
 
@@ -243,18 +273,22 @@ class RobotControllerApp:
 
     def button11_action(self):
         # Change the LED color to RED when the "RED" button is clicked
-        self.turn_off_leds()
         self.set_led_color(GPIO.HIGH, GPIO.LOW, GPIO.LOW)
 
     def button12_action(self):
         # Change the LED color to GREEN when the "GREEN" button is clicked
-        self.turn_off_leds()
         self.set_led_color(GPIO.LOW, GPIO.HIGH, GPIO.LOW)
 
     def button13_action(self):
         # Change the LED color to BLUE when the "BLUE" button is clicked
-        self.turn_off_leds()
         self.set_led_color(GPIO.LOW, GPIO.LOW, GPIO.HIGH)
+        
+    def button14_action(self):
+        self.spin_left(1)
+    
+    def button15_action(self):
+        self.spin_right(1)
+        
 
     def turn_off_leds(self):
         # Turn off all LEDs
@@ -268,13 +302,307 @@ class RobotControllerApp:
         GPIO.output(self.LED_G, green)
         GPIO.output(self.LED_B, blue)
         # Define the functions for each robot program option (1 to 40)
+        
+    #black_line_tracking_action    
     def robot_program_1_action(self):
-        messagebox.showinfo("Robot Program", "Robot Program 1 clicked")
+        try:
+            # Definition of motor pins
+            IN1 = 20
+            IN2 = 21
+            IN3 = 19
+            IN4 = 26
+            ENA = 16
+            ENB = 13
 
+            # Definition of button
+            key = 8
+
+            # TrackSensorLeftPin1 TrackSensorLeftPin2 TrackSensorRightPin1 TrackSensorRightPin2
+            # 3 5 4 18
+            TrackSensorLeftPin1 = 3
+            TrackSensorLeftPin2 = 5
+            TrackSensorRightPin1 = 4
+            TrackSensorRightPin2 = 18
+
+            # Set the GPIO port to BCM encoding mode.
+            GPIO.setmode(GPIO.BCM)
+
+            # Ignore warning information
+            GPIO.setwarnings(False)
+
+            # Motor pins are initialized into output mode
+            # Key pin is initialized into input mode
+            # Track sensor module pins are initialized into input mode
+            GPIO.setup(ENA, GPIO.OUT, initial=GPIO.HIGH)
+            GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(ENB, GPIO.OUT, initial=GPIO.HIGH)
+            GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(key, GPIO.IN)
+            GPIO.setup(TrackSensorLeftPin1, GPIO.IN)
+            GPIO.setup(TrackSensorLeftPin2, GPIO.IN)
+            GPIO.setup(TrackSensorRightPin1, GPIO.IN)
+            GPIO.setup(TrackSensorRightPin2, GPIO.IN)
+            # Set the PWM pin and frequency is 2000hz
+            pwm_ENA = GPIO.PWM(ENA, 2000)
+            pwm_ENB = GPIO.PWM(ENB, 2000)
+            pwm_ENA.start(0)
+            pwm_ENB.start(0)
+
+            def run(leftspeed, rightspeed):
+                GPIO.output(IN1, GPIO.HIGH)
+                GPIO.output(IN2, GPIO.LOW)
+                GPIO.output(IN3, GPIO.HIGH)
+                GPIO.output(IN4, GPIO.LOW)
+                pwm_ENA.ChangeDutyCycle(leftspeed)
+                pwm_ENB.ChangeDutyCycle(rightspeed)
+
+            def spin_right(leftspeed, rightspeed):
+                GPIO.output(IN1, GPIO.HIGH)
+                GPIO.output(IN2, GPIO.LOW)
+                GPIO.output(IN3, GPIO.LOW)
+                GPIO.output(IN4, GPIO.HIGH)
+                pwm_ENA.ChangeDutyCycle(leftspeed)
+                pwm_ENB.ChangeDutyCycle(rightspeed)
+
+            # Other tracking.py code
+
+            while True:
+                # Your tracking.py loop logic here
+                TrackSensorLeftValue1 = GPIO.input(TrackSensorLeftPin1)
+                TrackSensorLeftValue2 = GPIO.input(TrackSensorLeftPin2)
+                TrackSensorRightValue1 = GPIO.input(TrackSensorRightPin1)
+                TrackSensorRightValue2 = GPIO.input(TrackSensorRightPin2)
+
+                # 4 tracking pins level status
+                # 0 0 X 0
+                # 1 0 X 0
+                # 0 1 X 0
+                # Turn right in place, speed is 50, delay 80ms
+                # Handle right acute angle and right right angle
+                if (TrackSensorLeftValue1 == False or TrackSensorLeftValue2 == False) and TrackSensorRightValue2 == False:
+                    spin_right(35, 35)
+                    time.sleep(0.08)
+
+                # 4 tracking pins level status
+                # 0 X 0 0
+                # 0 X 0 1
+                # 0 X 1 0
+                # Turn right in place, speed is 50, delay 80ms
+                # Handle left acute angle and left right angle
+                elif TrackSensorLeftValue1 == False and (
+                        TrackSensorRightValue1 == False or TrackSensorRightValue2 == False):
+                    spin_left(35, 35)
+                    time.sleep(0.08)
+
+                # 0 X X X
+                # Left_sensor1 detected black line
+                elif TrackSensorLeftValue1 == False:
+                    spin_left(35, 35)
+
+                # X X X 0
+                # Right_sensor2 detected black line
+                elif TrackSensorRightValue2 == False:
+                    spin_right(35, 35)
+
+                # 4 tracking pins level status
+                # X 0 1 X
+                elif TrackSensorLeftValue2 == False and TrackSensorRightValue1 == True:
+                    run(0, 40)
+
+                # 4 tracking pins level status
+                # X 1 0 X
+                elif TrackSensorLeftValue2 == True and TrackSensorRightValue1 == False:
+                    run(40, 0)
+
+                # 4 tracking pins level status
+                # X 0 0 X
+                elif TrackSensorLeftValue2 == False and TrackSensorRightValue1 == False:
+                    run(45, 45)
+
+        except KeyboardInterrupt:
+            pass
+        pwm_ENA.stop()
+        pwm_ENB.stop()
+        GPIO.cleanup()
+
+        
     def robot_program_2_action(self):
-        messagebox.showinfo("Robot Program", "Robot Program 2 clicked")
+        # Definition of motor pins
+        IN1 = 20
+        IN2 = 21
+        IN3 = 19
+        IN4 = 26
+        ENA = 16
+        ENB = 13
+
+        # Definition of button
+        key = 8
+
+        # Definition of photoresistor pin
+        LdrSensorLeft = 7
+        LdrSensorRight = 6
+
+        # Set the GPIO port to BCM encoding mode.
+        GPIO.setmode(GPIO.BCM)
+
+        # Ignore warning information
+        GPIO.setwarnings(False)
+
+        def init():
+            global pwm_ENA
+            global pwm_ENB
+            GPIO.setup(ENA, GPIO.OUT, initial=GPIO.HIGH)
+            GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(ENB, GPIO.OUT, initial=GPIO.HIGH)
+            GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(key, GPIO.IN)
+            GPIO.setup(LdrSensorLeft, GPIO.IN)
+            GPIO.setup(LdrSensorRight, GPIO.IN)
+            # Set the PWM pin and frequency is 2000hz
+            pwm_ENA = GPIO.PWM(ENA, 2000)
+            pwm_ENB = GPIO.PWM(ENB, 2000)
+            pwm_ENA.start(0)
+            pwm_ENB.start(0)
+
+        # Advance
+        def run():
+            GPIO.output(IN1, GPIO.HIGH)
+            GPIO.output(IN2, GPIO.LOW)
+            GPIO.output(IN3, GPIO.HIGH)
+            GPIO.output(IN4, GPIO.LOW)
+            pwm_ENA.ChangeDutyCycle(50)
+            pwm_ENB.ChangeDutyCycle(50)
+
+        # Back
+        def back():
+            GPIO.output(IN1, GPIO.LOW)
+            GPIO.output(IN2, GPIO.HIGH)
+            GPIO.output(IN3, GPIO.LOW)
+            GPIO.output(IN4, GPIO.HIGH)
+            pwm_ENA.ChangeDutyCycle(50)
+            pwm_ENB.ChangeDutyCycle(50)
+
+        # Turn left
+        def left():
+            GPIO.output(IN1, GPIO.LOW)
+            GPIO.output(IN2, GPIO.LOW)
+            GPIO.output(IN3, GPIO.HIGH)
+            GPIO.output(IN4, GPIO.LOW)
+            pwm_ENA.ChangeDutyCycle(0)
+            pwm_ENB.ChangeDutyCycle(50)
+
+        # Turn right
+        def right():
+            GPIO.output(IN1, GPIO.HIGH)
+            GPIO.output(IN2, GPIO.LOW)
+            GPIO.output(IN3, GPIO.LOW)
+            GPIO.output(IN4, GPIO.LOW)
+            pwm_ENA.ChangeDutyCycle(50)
+            pwm_ENB.ChangeDutyCycle(0)
+
+        # Turn left in place
+        def spin_left():
+            GPIO.output(IN1, GPIO.LOW)
+            GPIO.output(IN2, GPIO.HIGH)
+            GPIO.output(IN3, GPIO.HIGH)
+            GPIO.output(IN4, GPIO.LOW)
+            pwm_ENA.ChangeDutyCycle(50)
+            pwm_ENB.ChangeDutyCycle(50)
+
+        # Turn right in place
+        def spin_right():
+            GPIO.output(IN1, GPIO.HIGH)
+            GPIO.output(IN2, GPIO.LOW)
+            GPIO.output(IN3, GPIO.LOW)
+            GPIO.output(IN4, GPIO.HIGH)
+            pwm_ENA.ChangeDutyCycle(50)
+            pwm_ENB.ChangeDutyCycle(50)
+
+        # Brake
+        def brake():
+            GPIO.output(IN1, GPIO.LOW)
+            GPIO.output(IN2, GPIO.LOW)
+            GPIO.output(IN3, GPIO.LOW)
+            GPIO.output(IN4, GPIO.LOW)
+
+        def key_scan():
+            while GPIO.input(key):
+                pass
+            while not GPIO.input(key):
+                time.sleep(0.01)
+                if not GPIO.input(key):
+                    time.sleep(0.01)
+                    while not GPIO.input(key):
+                        pass
+
+        # Delay 2s
+        time.sleep(2)
+
+        try:
+            init()
+            key_scan()
+            while True:
+                LdrSersorLeftValue = GPIO.input(LdrSensorLeft)
+                LdrSersorRightValue = GPIO.input(LdrSensorRight)
+
+                if LdrSersorLeftValue and LdrSersorRightValue:
+                    run()
+                elif LdrSersorLeftValue and not LdrSersorRightValue:
+                    spin_left()
+                    time.sleep(0.002)
+                elif LdrSersorRightValue and not LdrSersorLeftValue:
+                    spin_right()
+                    time.sleep(0.002)
+                elif not LdrSersorRightValue and not LdrSersorLeftValue:
+                    brake()
+
+        except KeyboardInterrupt:
+            pass
+
+        # Cleanup GPIO
+        pwm_ENA.stop()
+        pwm_ENB.stop()
+        GPIO.cleanup()
+
     def robot_program_3_action(self):
-        messagebox.showinfo("Robot Program", "Robot Program 3 clicked")
+        # Definition of RGB module pin
+        LED_R = 22
+        LED_G = 27
+        LED_B = 24
+
+        # Set the GPIO port to BCM encoding mode.
+        GPIO.setmode(GPIO.BCM)
+
+        # RGB pins are initialized into output mode
+        GPIO.setup(LED_R, GPIO.OUT)
+        GPIO.setup(LED_G, GPIO.OUT)
+        GPIO.setup(LED_B, GPIO.OUT)
+
+        try:
+            # Continuously change the LED color
+            while True:
+                # Red
+                self.set_led_color(GPIO.HIGH, GPIO.LOW, GPIO.LOW)
+                time.sleep(1)
+
+                # Green
+                self.set_led_color(GPIO.LOW, GPIO.HIGH, GPIO.LOW)
+                time.sleep(1)
+
+                # Blue
+                self.set_led_color(GPIO.LOW, GPIO.LOW, GPIO.HIGH)
+                time.sleep(1)
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            # Cleanup GPIO
+            GPIO.cleanup()
+
     def robot_program_4_action(self):
         messagebox.showinfo("Robot Program", "Robot Program 4 clicked")
     def robot_program_5_action(self):
@@ -382,28 +710,44 @@ class RobotControllerApp:
     
      # Function to move the robot forward
     def move_forward(self):
-        run(1)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+        self.pwm_ENA.ChangeDutyCycle(self.speed)
+        self.pwm_ENB.ChangeDutyCycle(self.speed)
+        time.sleep(1)
 
     # Function to move the robot backward
     def move_backward(self):
-        back(1)
+        self.back(1)
 
     # Function to turn the robot left
     def move_left(self):
-        left(1)
+        self.left(1)
 
     # Function to turn the robot right
     def move_right(self):
-        right(1)
+        self.right(1)
 
     # Function to stop the robot
     def stop_robot(self):
-        brake(1)
-    
+        self.brake(1)
+        
+    def whistle(self):
+        # Assuming you have defined the buzzer pin as buzzer (replace with your actual buzzer pin)
+        buzzer = 8
+
+        # Activate the buzzer
+        GPIO.output(buzzer, GPIO.LOW)
+        time.sleep(0.1)
+        GPIO.output(buzzer, GPIO.HIGH)
+        time.sleep(0.001)
+        
+
     def cleanup_gpio(self):
         # Cleanup GPIO before exiting
         GPIO.cleanup()
-        
         
         
 
